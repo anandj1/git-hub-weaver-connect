02d7d9068c83177756a76607e96bc82fcea75d98
Anand <131908427+anandj1@users.noreply.github.com>
2025-06-07 14:47:55 +0530

    Update wsgi.py

--- projectdir/wsgi.py
+++ projectdir/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectdir.settings')
+# Set the default settings module for Railway deployment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectdir.railway')
 
 application = get_wsgi_application()
